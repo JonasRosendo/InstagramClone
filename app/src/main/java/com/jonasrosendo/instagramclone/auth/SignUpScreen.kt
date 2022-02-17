5397bfb84b98ab2fb8e20560c3aa9972c351294c
Jonas Rosendo <jonasrosendo@live.com>
2022-02-17 13:54:28 -0300

    cleaning up unused imports

--- app/src/main/java/com/jonasrosendo/instagramclone/auth/SignUpScreen.kt
+++ app/src/main/java/com/jonasrosendo/instagramclone/auth/SignUpScreen.kt
@@ -19,10 +19,8 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.jonasrosendo.instagramclone.InstagramViewModel
 import com.jonasrosendo.instagramclone.R
@@ -92,11 +90,11 @@ fun SignUpScreen(navController: NavController, viewModel: InstagramViewModel) {
 
             Button(
                 onClick = {
-                          viewModel.signUp(
-                              usernameState.value.text,
-                              emailState.value.text,
-                              passwordState.value.text
-                          )
+                    viewModel.signUp(
+                        usernameState.value.text,
+                        emailState.value.text,
+                        passwordState.value.text
+                    )
                 },
                 modifier = fieldModifier
             ) {
